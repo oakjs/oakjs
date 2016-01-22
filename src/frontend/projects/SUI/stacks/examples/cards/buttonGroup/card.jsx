67d7488534273526e4befa7265a8226816b7a4dc
owenoak <owenoak@gmail.com>
2016-01-21 19:35:58 -0500

    using <$.Foo> pattern removes annoying need to pull components into scope converted to static renderer for card wrapper

--- src/frontend/projects/SUI/stacks/examples/cards/buttonGroup/card.jsx
+++ src/frontend/projects/SUI/stacks/examples/cards/buttonGroup/card.jsx
@@ -2,222 +2,218 @@
 import React from "react";
 
 // use stack components
-import components from "../../components";
+import $ from "../../components";
 
-export default class ButtonGroupCard extends React.Component {
-  render() {
-    const { Card, Example, Page, PageSidebar, Section } = components;
-    const { Button, ButtonGroup, Container, Conditional, Icon, Message, Segment, Spacer } = components;
-    return (
-      <Card id="ButtonGroup" title="<ButtonGroup> Examples">
-        <PageSidebar/>
-        <Page title="ButtonGroup" description="A ButtonGroup encapsulates a set of Button elements, generally to provide consistent styling.">
-          <Section title="Appearance">
-            <Example title="Normal">
-              <ButtonGroup>
-                <Button title="One"/>
-                <Button title="Two"/>
-                <Button title="Three"/>
-              </ButtonGroup>
-            </Example>
+export default function ButtonGroup(props) {
+  return (
+    <$.Card id="ButtonGroup" title="<ButtonGroup> Examples">
+      <$.PageSidebar/>
+      <$.Page title="ButtonGroup" description="A ButtonGroup encapsulates a set of Button elements, generally to provide consistent styling.">
+        <$.Section title="Appearance">
+          <$.Example title="Normal">
+            <$.ButtonGroup>
+              <$.Button title="One"/>
+              <$.Button title="Two"/>
+              <$.Button title="Three"/>
+            </$.ButtonGroup>
+          </$.Example>
 
-            <Example title="Compact">
-              <ButtonGroup compact>
-                <Button>One</Button>
-                <Button>Two</Button>
-              </ButtonGroup>
-            </Example>
+          <$.Example title="Compact">
+            <$.ButtonGroup compact>
+              <$.Button>One</$.Button>
+              <$.Button>Two</$.Button>
+            </$.ButtonGroup>
+          </$.Example>
 
-            <Example title="Icon Buttons">
-              <ButtonGroup appearance="icon">
-                <Button icon="align left"/>
-                <Button icon="align center"/>
-                <Button icon="align right"/>
-                <Button icon="align justify"/>
-              </ButtonGroup>
-              <Spacer inline/>
-              <ButtonGroup appearance="icon">
-                <Button icon="bold"/>
-                <Button icon="underline"/>
-                <Button icon="text width"/>
-              </ButtonGroup>
-            </Example>
+          <$.Example title="Icon Buttons">
+            <$.ButtonGroup appearance="icon">
+              <$.Button icon="align left"/>
+              <$.Button icon="align center"/>
+              <$.Button icon="align right"/>
+              <$.Button icon="align justify"/>
+            </$.ButtonGroup>
+            <$.Spacer inline/>
+            <$.ButtonGroup appearance="icon">
+              <$.Button icon="bold"/>
+              <$.Button icon="underline"/>
+              <$.Button icon="text width"/>
+            </$.ButtonGroup>
+          </$.Example>
 
-            <Example title="Labeled Icon Buttons">
-              <ButtonGroup appearance="vertical labeled icon">
-                <Button icon="play" title="Play"/>
-                <Button icon="pause" title="Pause"/>
-                <Button icon="shuffle" title="Shuffle"/>
-              </ButtonGroup>
-            </Example>
+          <$.Example title="Labeled Icon Buttons">
+            <$.ButtonGroup appearance="vertical labeled icon">
+              <$.Button icon="play" title="Play"/>
+              <$.Button icon="pause" title="Pause"/>
+              <$.Button icon="shuffle" title="Shuffle"/>
+            </$.ButtonGroup>
+          </$.Example>
 
-            <Example title="Mixed Group">
-              <ButtonGroup>
-                <Button icon="left chevron" appearance="labeled icon" title="Back"/>
-                <Button icon="stop" title="Stop"/>
-                <Button icon="right chevron" appearance="right labeled icon" title="Forward"/>
-              </ButtonGroup>
-            </Example>
+          <$.Example title="Mixed Group">
+            <$.ButtonGroup>
+              <$.Button icon="left chevron" appearance="labeled icon" title="Back"/>
+              <$.Button icon="stop" title="Stop"/>
+              <$.Button icon="right chevron" appearance="right labeled icon" title="Forward"/>
+            </$.ButtonGroup>
+          </$.Example>
 
-            <Example title="Conditionals">
-              <ButtonGroup>
-                <Button>Cancel</Button>
-                <Conditional/>
-                <Button color="positive">Save</Button>
-              </ButtonGroup>
-            </Example>
+          <$.Example title="Conditionals">
+            <$.ButtonGroup>
+              <$.Button>Cancel</$.Button>
+              <$.Conditional/>
+              <$.Button color="positive">Save</$.Button>
+            </$.ButtonGroup>
+          </$.Example>
 
-            <Example title="Colored Buttons">
-              <ButtonGroup color="blue">
-                <Button title="One"/>
-                <Button title="Two"/>
-                <Button title="Three"/>
-              </ButtonGroup>
-            </Example>
+          <$.Example title="Colored Buttons">
+            <$.ButtonGroup color="blue">
+              <$.Button title="One"/>
+              <$.Button title="Two"/>
+              <$.Button title="Three"/>
+            </$.ButtonGroup>
+          </$.Example>
 
-            <Example title="Basic Buttons">
-              <ButtonGroup appearance="basic">
-                <Button title="One"/>
-                <Button title="Two"/>
-                <Button title="Three"/>
-              </ButtonGroup>
+          <$.Example title="Basic Buttons">
+            <$.ButtonGroup appearance="basic">
+              <$.Button title="One"/>
+              <$.Button title="Two"/>
+              <$.Button title="Three"/>
+            </$.ButtonGroup>
 
-              <Spacer/>
-              <ButtonGroup>
-                <Button title="One" color="red" appearance="basic"/>
-                <Button title="Two" color="blue" appearance="basic"/>
-                <Button title="Three" color="green" appearance="basic"/>
-              </ButtonGroup>
-            </Example>
+            <$.Spacer/>
+            <$.ButtonGroup>
+              <$.Button title="One" color="red" appearance="basic"/>
+              <$.Button title="Two" color="blue" appearance="basic"/>
+              <$.Button title="Three" color="green" appearance="basic"/>
+            </$.ButtonGroup>
+          </$.Example>
 
 
-            <Example title="Vertical">
-              <ButtonGroup appearance="vertical">
-                <Button title="One"/>
-                <Button title="Two"/>
-                <Button title="Three"/>
-              </ButtonGroup>
+          <$.Example title="Vertical">
+            <$.ButtonGroup appearance="vertical">
+              <$.Button title="One"/>
+              <$.Button title="Two"/>
+              <$.Button title="Three"/>
+            </$.ButtonGroup>
 
-              <Spacer inline/>
-              <ButtonGroup appearance="vertical" color="blue">
-                <Button title="One"/>
-                <Button title="Two"/>
-                <Button title="Three"/>
-              </ButtonGroup>
+            <$.Spacer inline/>
+            <$.ButtonGroup appearance="vertical" color="blue">
+              <$.Button title="One"/>
+              <$.Button title="Two"/>
+              <$.Button title="Three"/>
+            </$.ButtonGroup>
 
-              <Spacer inline/>
-              <ButtonGroup appearance="vertical">
-                <Button title="One" color="red"/>
-                <Button title="Two" color="blue"/>
-                <Button title="Three" color="green"/>
-              </ButtonGroup>
+            <$.Spacer inline/>
+            <$.ButtonGroup appearance="vertical">
+              <$.Button title="One" color="red"/>
+              <$.Button title="Two" color="blue"/>
+              <$.Button title="Three" color="green"/>
+            </$.ButtonGroup>
 
-              <Spacer inline size="large"/>
-              <ButtonGroup appearance="vertical basic">
-                <Button title="One"/>
-                <Button title="Two"/>
-                <Button title="Three"/>
-              </ButtonGroup>
+            <$.Spacer inline size="large"/>
+            <$.ButtonGroup appearance="vertical basic">
+              <$.Button title="One"/>
+              <$.Button title="Two"/>
+              <$.Button title="Three"/>
+            </$.ButtonGroup>
 
-              <Spacer inline/>
-              <ButtonGroup appearance="vertical basic" color="blue">
-                <Button title="One"/>
-                <Button title="Two"/>
-                <Button title="Three"/>
-              </ButtonGroup>
+            <$.Spacer inline/>
+            <$.ButtonGroup appearance="vertical basic" color="blue">
+              <$.Button title="One"/>
+              <$.Button title="Two"/>
+              <$.Button title="Three"/>
+            </$.ButtonGroup>
 
-              <Spacer inline/>
-              <ButtonGroup appearance="vertical basic">
-                <Button title="One" color="red" appearance="basic"/>
-                <Button title="Two" color="blue" appearance="basic"/>
-                <Button title="Three" color="green" appearance="basic"/>
-              </ButtonGroup>
-            </Example>
-          </Section>
+            <$.Spacer inline/>
+            <$.ButtonGroup appearance="vertical basic">
+              <$.Button title="One" color="red" appearance="basic"/>
+              <$.Button title="Two" color="blue" appearance="basic"/>
+              <$.Button title="Three" color="green" appearance="basic"/>
+            </$.ButtonGroup>
+          </$.Example>
+        </$.Section>
 
-            <Section title="Sizing">
-            <Example title="Group Sizes">
-              <ButtonGroup size="tiny">
-                <Button title="We"/>
-                <Button title="Are"/>
-                <Button title="Tiny!"/>
-              </ButtonGroup>
+          <$.Section title="Sizing">
+          <$.Example title="Group Sizes">
+            <$.ButtonGroup size="tiny">
+              <$.Button title="We"/>
+              <$.Button title="Are"/>
+              <$.Button title="Tiny!"/>
+            </$.ButtonGroup>
 
-              <Spacer/>
-              <ButtonGroup size="large" appearance="basic icon">
-                <Button icon="file"/>
-                <Button icon="save"/>
-                <Button icon="upload"/>
-                <Button icon="download"/>
-              </ButtonGroup>
+            <$.Spacer/>
+            <$.ButtonGroup size="large" appearance="basic icon">
+              <$.Button icon="file"/>
+              <$.Button icon="save"/>
+              <$.Button icon="upload"/>
+              <$.Button icon="download"/>
+            </$.ButtonGroup>
 
-              <Spacer/>
-              <ButtonGroup size="huge">
-                <Button title="One"/>
-                <Conditional/>
-                <Button title="Two"/>
-                <Conditional/>
-                <Button title="Three"/>
-              </ButtonGroup>
-            </Example>
+            <$.Spacer/>
+            <$.ButtonGroup size="huge">
+              <$.Button title="One"/>
+              <$.Conditional/>
+              <$.Button title="Two"/>
+              <$.Conditional/>
+              <$.Button title="Three"/>
+            </$.ButtonGroup>
+          </$.Example>
 
-            <Example title="Fluid">
-              <ButtonGroup appearance="fluid">
-                <Button title="Overview"/>
-                <Button title="Specs"/>
-                <Button title="Warranty"/>
-                <Button title="Reviews"/>
-                <Button title="Support"/>
-              </ButtonGroup>
-            </Example>
+          <$.Example title="Fluid">
+            <$.ButtonGroup appearance="fluid">
+              <$.Button title="Overview"/>
+              <$.Button title="Specs"/>
+              <$.Button title="Warranty"/>
+              <$.Button title="Reviews"/>
+              <$.Button title="Support"/>
+            </$.ButtonGroup>
+          </$.Example>
 
-            <Example title="Equal Width">
-              <ButtonGroup count={5}>
-                <Button title="Overview"/>
-                <Button title="Specs"/>
-                <Button title="Warranty"/>
-                <Button title="Reviews"/>
-                <Button title="Support"/>
-              </ButtonGroup>
-              <Spacer/>
-              <ButtonGroup count={3}>
-                <Button title="Overview"/>
-                <Button title="Specs"/>
-                <Button title="Support"/>
-              </ButtonGroup>
-            </Example>
-          </Section>
+          <$.Example title="Equal Width">
+            <$.ButtonGroup count={5}>
+              <$.Button title="Overview"/>
+              <$.Button title="Specs"/>
+              <$.Button title="Warranty"/>
+              <$.Button title="Reviews"/>
+              <$.Button title="Support"/>
+            </$.ButtonGroup>
+            <$.Spacer/>
+            <$.ButtonGroup count={3}>
+              <$.Button title="Overview"/>
+              <$.Button title="Specs"/>
+              <$.Button title="Support"/>
+            </$.ButtonGroup>
+          </$.Example>
+        </$.Section>
 
-          <Section title="Attachment">
-            <Example title="Floated">
-              <Segment clearing appearance="unpadded basic">
-                <ButtonGroup float="left">
-                  <Button icon="left arrow"/>
-                  <Button icon="right arrow"/>
-                </ButtonGroup>
-                <ButtonGroup float="right">
-                  <Button icon="left arrow"/>
-                  <Button icon="right arrow"/>
-                </ButtonGroup>
-              </Segment>
-            </Example>
+        <$.Section title="Attachment">
+          <$.Example title="Floated">
+            <$.Segment clearing appearance="unpadded basic">
+              <$.ButtonGroup float="left">
+                <$.Button icon="left arrow"/>
+                <$.Button icon="right arrow"/>
+              </$.ButtonGroup>
+              <$.ButtonGroup float="right">
+                <$.Button icon="left arrow"/>
+                <$.Button icon="right arrow"/>
+              </$.ButtonGroup>
+            </$.Segment>
+          </$.Example>
 
 
-            <Example title="Vertically Attached">
-              <ButtonGroup attached="top">
-                <Button title="One"/>
-                <Button title="Two"/>
-              </ButtonGroup>
-              <Segment appearance="attached"><p>Segment Content Here</p></Segment>
-              <ButtonGroup attached="bottom">
-                <Button title="One"/>
-                <Button title="Two"/>
-              </ButtonGroup>
-            </Example>
-          </Section>
+          <$.Example title="Vertically Attached">
+            <$.ButtonGroup attached="top">
+              <$.Button title="One"/>
+              <$.Button title="Two"/>
+            </$.ButtonGroup>
+            <$.Segment appearance="attached"><p>Segment Content Here</p></$.Segment>
+            <$.ButtonGroup attached="bottom">
+              <$.Button title="One"/>
+              <$.Button title="Two"/>
+            </$.ButtonGroup>
+          </$.Example>
+        </$.Section>
 
-        </Page>
-      </Card>
-    );
-  }
+      </$.Page>
+    </$.Card>
+  );
 }
--- src/frontend/projects/SUI/stacks/examples/cards/dropdown/card.jsx
+++ src/frontend/projects/SUI/stacks/examples/cards/dropdown/card.jsx
@@ -2,28 +2,24 @@
 import React from "react";
 
 // use stack components
-import components from "../../components";
+import $ from "../../components";
 
-export default class ButtonCard extends React.Component {
-  render() {
-    const { Card, Example, Page, PageSidebar, Section } = components;
-    const { Dropdown, Divider, Menu, MenuItem, MenuHeader } = components;
-    return (
-      <Card id="Button" title="<Button> Examples">
-        <PageSidebar/>
-        <Page title="Dropdown" description="...TODOC...">
-          <Section>
-            <Example title="Basic dropdown">
-              <Dropdown placeholder="Foo" appearance="button" selectable multiSelect icon="heart" showArrow={true}>
-                <Menu>
-                  <MenuItem label="Yah"/>
-                  <MenuItem label="Hoo"/>
-                </Menu>
-              </Dropdown>
-            </Example>
-          </Section>
-        </Page>
-      </Card>
-    );
-  }
+export default function Dropdown(props) {
+  return (
+    <$.Card id="Dropdown" title="<Dropdown> Examples">
+      <$.PageSidebar/>
+      <$.Page title="Dropdown" description="...TODOC...">
+        <$.Section title="Todo">
+          <$.Example title="Basic dropdown">
+            <$.Dropdown placeholder="Foo" appearance="button" selectable multiSelect icon="heart" showArrow={true}>
+              <$.Menu>
+                <$.MenuItem label="Yah"/>
+                <$.MenuItem label="Hoo"/>
+              </$.Menu>
+            </$.Dropdown>
+          </$.Example>
+        </$.Section>
+      </$.Page>
+    </$.Card>
+  );
 }
