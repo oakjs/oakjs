9b9468365b5d9f80a31b2b8ceb0a7bf077efaf70
owenoak <owenoak@gmail.com>
2016-02-18 09:24:58 -0500

    util/browser (needs unit tests)

---
+++ src/oak/util/browser.js
@@ -0,0 +1,32 @@
+//////////////////////////////
+// Browser functionality
+//////////////////////////////
+
+import ids from "./ids";
+
+
+// Create a stylesheet and install it if we're in the browser.
+// If you pass an `id`, you can call this again to change the stylesheet later .
+// Returns `true` if we're in the browser and this works, otherwise returns `false`.
+export function createStylesheet(styles, id = "STYLE-"+ids.generateRandomId()) {
+  if (typeof $ === "undefined") return false;
+
+  const $sheet = "<style type='text/css' id="+id+">" + styles + "</style>";
+
+  let $existing = $(`style#${id}`);
+  if ($existing.length) {
+    $existing.replaceWith($sheet);
+  }
+  else {
+    $("head").append($sheet);
+  }
+}
+
+// Remove a stylesheet specified by id.
+export function removeStylesheet(id) {
+  $(`style#${id}`).remove();
+}
+
+
+// Export all as a single object.
+export default exports;
