6f018599e8b6161042afe39aa1552c6d2afad286
owenoak <owenoak@gmail.com>
2016-02-18 09:28:02 -0500

    oak/index.js loads all of the misc bits and assigns them to `oak` singleton

--- src/oak/index.js
+++ src/oak/index.js
@@ -1,22 +1,31 @@
+import oak from "./oak";
+
 // Project/Stack/Card
 export Card from "./Card";
-export Parser from "./Parser";
+export CardController from "./CardController";
+export Eventful from "./Eventful";
+export JSXElement from "./JSXElement";
+export Loadable from "./Loadable";
+export Mutable from "./Mutable";
 export Project from "./Project";
+export Savable from "./Savable";
 export Stack from "./Stack";
+
+export astParser from "./util/astParser";
+export browser from "./util/browser";
+export ajax from "./util/ajax";
+export ids from "./util/ids";
+export object from "./util/objectUtil";
+
+export parserTest from "./parserTest";
 
 // Card components.
 // NOTE: in a separate file so we don't get a circular dependency.
-import * as cardComponents from "./components";
-Object.assign(exports, cardComponents);
+import * as components from "./components";
+Object.assign(exports, components);
 
+// assign everything we've loaded to the oak singleton
+Object.assign(oak, exports);
 
-// get all of the components
-const components = Object.keys(exports);
-
-// Export the root of the oak namespace
-export oak from "./oak";
-
-// Attach all of the components to the oak namespace
-components.forEach(key => exports.oak[key] = exports[key]);
-
+// export the oak object as the default
 export default oak;
--- src/oak/oak.js
+++ src/oak/oak.js
@@ -6,15 +6,14 @@
 //
 //////////////////////////////
 
-import React from "react";
-
-
 // Create with an IIFE for reflection in debugger.
 const oak = new (function oak() {})();
 
 export default oak;
 
-
 // GLOBAL EXPORT OF OAK SINGLETON
 if (typeof window !== "undefined") window.oak = oak;
 if (typeof global !== "undefined") global.oak = oak;
+
+
+
