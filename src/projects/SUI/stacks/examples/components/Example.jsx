d5e24e74b56a87996b9dcfa0226d33095426f768
owenoak <owenoak@gmail.com>
2016-02-18 23:43:38 -0500

    a couple of component .jsxe's

--- src/projects/SUI/stacks/examples/components/Example.jsx
+++ src/projects/SUI/stacks/examples/components/Example.jsx
@@ -1,7 +1,6 @@
 import React, { PropTypes } from "react";
 import classNames from "classnames";
 
-import InfoHint from "./InfoHint";
 import "./Example.css";
 
 function Example(props, { components: c }) {
