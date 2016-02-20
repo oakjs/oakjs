fb35c551a55b907ca2fcb1dcfb9366f0f28c76ee
owenoak <owenoak@gmail.com>
2016-02-20 16:59:27 -0500

    API error message display

--- src/client/oak-roots/API.js
+++ src/client/oak-roots/API.js
@@ -32,6 +32,10 @@
 //////////////////////////////
 
 
+// TODO: use the fact that we know loading time to avoid reloading?
+//  - can check controller.lastLoadTime and set If-Modified-Since header
+//  - will have to process a 304 specially...
+
 import objectUtil from "./util/object";
 
 // Ensure that global `fetch` routine is present.
@@ -55,10 +59,11 @@ export default class API {
   // Cover routine for standard Web `fetch` API which considers a non-200ish response an error.
   //
   // Note:  ALL API routines should eventually get down to this one method.
-  //        That allows us to batch requests to the server if desired.
+  //        This will allow us to batch requests to the server if desired.
   //
-  fetch(path, fetchParams) {
-    return nativeFetch(path, fetchParams)
+  // If you pass an `errorMessage`, we'll log any error found and re-throw it.
+  fetch(url, fetchParams, errorMessage) {
+    let promise = nativeFetch(url, fetchParams)
       .then(response => {
         if (!response.ok) {
           const error = new Error(response.statusText);
@@ -70,6 +75,17 @@ export default class API {
         }
         return response;
       });
+
+    // if we got an error message, catch and log any errors
+    // but do NOT stop normal processing
+    if (errorMessage) {
+      promise = promise.catch(error => {
+        this.logError({ errorMessage, error, url, fetchParams });
+        throw error;
+      });
+    }
+
+    return promise;
   }
 
 
@@ -77,49 +93,44 @@ export default class API {
   // Getters
   //////////////////////////////
 
-  // Get a text resource at `path`.
+  // Get a text resource at `url`.
   // Returns a promise which yields the reponse as a text string.
-  getText(path, fetchParams) {
-    return this.fetch(path, fetchParams).then(response => response.text());
+  // If you pass an `errorMessage`, we'll log any error found and re-throw it.
+  getText(url, fetchParams, errorMessage) {
+    return this.fetch(url, fetchParams, errorMessage)
+            .then(response => {
+              return response.text()
+                .catch(error => {
+                  this.logError({ errorMessage, error, detail: "Error extracting text from response", url, fetchParams, response });
+                  throw error;
+                });
+            });
   }
 
-  // Get a JSON resource at `path`.
+  // Get a JSON resource at `url`.
   // Returns a promise which yields the reponse as a json object.
   // Throws if the json is not valid.
-  getJSON(path, fetchParams) {
-    return this.fetch(path, fetchParams).then(response => response.json());
-  }
-
-  // Get resource at `path` returning appropriate result type:
-  //    Content-Type          Returned
-  //    --------------------|---------------------
-  //    "application/json"  | `response.json()`
-  //    (anything else)     | `response.text()`
-  //
-  get(path, fetchParams) {
-    return API.fetch(input, fetchParams)
-      .then(response => {
-        // Figure out the content type:
-        //  - if they specific an explicit type in the fetchParams, use that
-        //  - otherwise trust what the server told us
-        const contentType = API.getContentType(fetchParams) || API.getContentType(response);
-        switch (contentType) {
-          case "application/json":  return response.json();
-          // TODO...
-          default:                  return response.text();
-        }
-      });
+  // If you pass an `errorMessage`, we'll log any error found and re-throw it.
+  getJSON(url, fetchParams, errorMessage) {
+    return this.fetch(url, fetchParams, errorMessage)
+            .then(response => {
+              return response.json()
+                .catch(error => {
+                  this.logError({ errorMessage, error, detail: "Error parsing JSON from response", url, fetchParams, response });
+                  throw error;
+                });
+            })
   }
 
   //////////////////////////////
   // Posters
   //////////////////////////////
 
-  // Post something to the server at `path`.
+  // Post something to the server at `url`.
   // If `body` is not a string, we'll `JSON.strigify()` it.
   //
   // Throws and logs to console if if `JSON.stringify()` fails.
-  post(path, body, fetchParams = {}) {
+  post(url, body, fetchParams = {}, errorMessage) {
     if (!fetchParams.method) fetchParams.method = "POST";
     if (body != null) {
       if (typeof body === "string") fetchParams.body = body;
@@ -128,16 +139,14 @@ export default class API {
           fetchParams.body = JSON.stringify(body);
         }
         catch (error) {
-          console.group(`api.post(): error converting json to string`);
-          console.error(error);
-          conosle.log("body: ", body);
-          console.groupEnd();
-
+          error.detail = "Error JSON.stringifying body";
+          const message = errorMessage || "Error posting to the server";
+          this.logError({ errorMessage: message, error, url, fetchParams, body });
           return Promise.reject(error);
         }
       }
     }
-    return this.fetch(path, fetchParams);
+    return this.fetch(url, fetchParams, errorMessage);
   }
 
 
@@ -145,6 +154,48 @@ export default class API {
   //  Utilities
   //////////////////////////////
 
+  // Set a header in a `fetchParams`.
+  // Works if we have a header or not, if header has a `set` or not.
+  setHeader(fetchParams = {}, headerName, value) {
+    if (!fetchParams.headers) fetchParams.headers = {};
+    if (fetchParams.headers.set) {
+      fetchParams.headers.set(headerName, value);
+    }
+    else {
+      fetchParams.headers[headerName] = value;
+    }
+    return fetchParams;
+  }
+
+  // Given a timestamp and a possible fetchParams object,
+  //  add the timestamp as a `If-Modified-Since` header
+  addModifiedHeader(fetchParams, timestamp) {
+    const serverTimestamp = this.getTimestampForServer(timestamp);
+    if (serverTimestamp) this.setHeader(fetchParams, "If-Modified-Since", serverTimestamp);
+    return fetchParams;
+  }
+
+  // Parse a timestamp into a date given:
+  //  - a `Date`
+  //  - a timestamp in msec, or
+  //  - a date string
+  // Returns `undefined` if we can't figure it out.
+  parseTimestamp(timestamp) {
+    let date = timestamp;
+    if (typeof timestamp === "number") date = new Date(timestamp);
+    else if (typeof timestamp === "string") date = Date.parse(timestamp);
+    if (date instanceof Date && isFinite(date)) return date;
+    return undefined;
+  }
+
+  // Given:  a `Date`, a timestamp in msec or a date string
+  //  return a timestamp as the server expects it.
+  // Returns `undefined` if we can't figure it out.
+  getTimestampForServer(timestamp) {
+    const date = this.parseTimestamp(timestamp);
+    if (date) return date.toUTCString();
+    return undefined;
+  }
 
   // Given a map of `{ key: loadingPromise }`,
   //  return a Promise which resolves to `{ key: result }`
@@ -157,6 +208,19 @@ export default class API {
         results.forEach((result, index) => map[keys[index]] = result);
         return map;
       });
+  }
+
+  // Log a response or other error
+  logError({errorMessage, error, ...params}) {
+    console.group(errorMessage);
+    console.error(error);
+    for (let key in params) {
+      console.log(key+":", params[key]);
+    }
+    for (let key in error) {
+      console.log(key+":", error[key]);
+    }
+    console.groupEnd();
   }
 
 
