05b81d5d89ee9e6c91ec304eb012c9dd808cff55
owenoak <owenoak@gmail.com>
2016-03-26 13:40:49 -0400

    `visible` prop for Buttons (required changing them to a stateful Component)

--- src/themes/SUI/Buttons.jsx
+++ src/themes/SUI/Buttons.jsx
@@ -2,44 +2,54 @@
 //
 //	<Buttons> component for use with SemanticUI
 //
+// TODO: add `visible` to examples stack
+// TODO: `enabled`
 //////////////////////////////
 
 import React, { PropTypes } from "react";
 import { classNames } from "oak-roots/util/react";
 
+import SUIComponent from "./SUIComponent";
 import { getNameForNumber, getFloatedClass, getAttachedClass } from "./constants";
 
-function SUIButtons(props) {
-  const {
-    appearance, size, color, count, floated, style,
-    children,
-    ...elementProps
-  } = props;
+export default class SUIButtons extends SUIComponent {
+  static propTypes = {
+    id: PropTypes.string,
+    className: PropTypes.string,
+    style: PropTypes.object,
+    children: PropTypes.any,
 
-  elementProps.className = classNames(
-    elementProps.className,
-    "ui", appearance, size, color,
-    getNameForNumber(count), getFloatedClass(floated),
-    "buttons"
-  );
+    visible: PropTypes.bool,
+    appearance: PropTypes.string,
+    size: PropTypes.string,
+    color: PropTypes.string,
+    count: PropTypes.number,
+    floated: PropTypes.string,
+  };
 
-  return <div {...elementProps}>{children}</div>;
+  static defaultProps = {
+    visible: true
+  }
+
+  render() {
+    const {
+      visible, appearance, size, color, count, floated, style,
+      children,
+      ...elementProps
+    } = this.props;
+
+    console.warn(visible, color);
+    if (!visible) return null;
+
+    elementProps.className = classNames(
+      elementProps.className,
+      "ui", appearance, size, color,
+      getNameForNumber(count), getFloatedClass(floated),
+      "buttons"
+    );
+
+    return <div {...elementProps}>{children}</div>;
+  }
 }
-
-SUIButtons.propTypes = {
-  id: PropTypes.string,
-  className: PropTypes.string,
-  style: PropTypes.object,
-  children: PropTypes.any,
-
-  appearance: PropTypes.string,
-  size: PropTypes.string,
-  color: PropTypes.string,
-  count: PropTypes.number,
-  floated: PropTypes.string,
-};
-
-// add render() method so we get hot code reload.
-SUIButtons.render = Function.prototype;
 
 export default SUIButtons;
