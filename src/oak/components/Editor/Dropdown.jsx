0a09109b4a9dc7d01aff2e0fbc03e0e900f3591d
owenoak <owenoak@gmail.com>
2016-09-04 19:20:53 -0400

    <Editor-Dropdown>:  delay on onDropdownChanged, normalize to array earlier if props.multiple

--- src/oak/components/Editor/Dropdown.jsx
+++ src/oak/components/Editor/Dropdown.jsx
@@ -6,28 +6,14 @@
 //////////////////////////////
 
 import React, { PropTypes } from "react";
-import ReactDOM from "react-dom";
 
-import { classNames, unknownProps, mergeProps, stringOrFn, boolOrFn } from "oak-roots/util/react";
-import { definedProperties } from "oak-roots/util/object";
+import { debounce } from "oak-roots/util/decorators";
 
 import SUIDropdown from "themes/SUI/Dropdown";
 
 import Control from "./Control";
 
-
-// "<Select>" Control subclass.
-// Specify `@options` or `@values` (e.g. from schema) as:
-//  - comma-separated string of values:     "a,b"
-//	- array of scalar values								["a", "b"]
-//	- array of arrays `[ key, "label" ]`		[ ["a", "AAA", "b": "BBB" ] ]
-//	- map of `{ key => label }`							{ a: "AAA", b: "BBB" }
-//
-// If select is not `@required`, we'll automatically add an empty option to the select,
-//	whose value will be `undefined` when it is selected.
-//
-// Note: the values you get back will NOT necessarily be strings if your values/options are no strings,
-//				e.g.  <Select values={[ true, false ]}
+// "<Dropdown>" Control subclass based on <SUI-Dropdown>.
 export default class Dropdown extends Control {
 	// Add <Select> specific propTypes
 	static propTypes = {
@@ -39,6 +25,12 @@ export default class Dropdown extends Control {
 		multiple: PropTypes.bool,							// multi-select?
 		search: PropTypes.bool, 							// search-style: allows them to type
 		selection: PropTypes.bool,  					// looks like a <select>
+
+		delimiter: PropTypes.string,          // Delimiter for multiple options specified as string
+	}
+
+	static defaultProps = {
+	  delimiter: ","
 	}
 
 	// Properties passed to control.
@@ -47,8 +39,34 @@ export default class Dropdown extends Control {
 	];
 
 
+  // Return true if 2 values are the same.
+  // Arrays will be joined
+  valuesAreTheSame(value1, value2) {
+    if (Array.isArray(value1)) value1 = value1.join(this.props.delimiter);
+    if (Array.isArray(value2)) value2 = value2.join(this.props.delimiter);
+    return value1 === value2;
+  }
+
+  getCurrentValue(props) {
+    const value = super.getCurrentValue(props);
+    // if multi-select, convert non-arrays to arrays
+    if (props.multiple && !Array.isArray(value)) {
+      if (typeof value === "string") {
+        if (value === "") return [];
+        return value.split(props.delimiter);
+      }
+      return [value];
+    }
+    return value;
+  }
+
   // Custom event for dropdown change which we pass up to the control.
+  @debounce(10)
   onDropdownChanged(value) {
+    if (this.valuesAreTheSame(value, this._renderValue)) {
+      return console.warn("onDropdownChanged: skipping change with same value ", value);
+    }
+
     if (this._props.multiple) {
       if (typeof value === "string" && value !== "") value = value.split(",");
     }
@@ -58,9 +76,14 @@ export default class Dropdown extends Control {
   renderControl(props) {
     const controlProps = this.getControlProps(props);
 
-    // pass `options` and/or `enum` down as `items`
-    controlProps.items = (props.options || props["enum"]);
+    // pass `options` and/or `enum` down as `items` if items wasn't specified
+    if (!controlProps.items) controlProps.items = (props.options || props["enum"]);
     controlProps.onChange = (value) => this.onDropdownChanged(value);
+
+    controlProps.label = { duration: 0 };
+
+    // remember the value we were rendered with to avoid endless loop in `onDropdownChanged`
+    this._renderValue = props.value;
 
     return React.createElement(SUIDropdown, controlProps);
   }
