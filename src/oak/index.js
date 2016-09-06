e50805cfbd5a6ac1c9910a6a1723c60d35f0cf54
owenoak <owenoak@gmail.com>
2016-09-05 22:16:53 -0400

    remove outdated parserTest

--- src/oak/index.js
+++ src/oak/index.js
@@ -4,9 +4,6 @@ export roots from "oak-roots";
 export api from "./api";
 export classes from "./classes";
 
-// Test stuff
-//export parserTest from "./parserTest";
-
 // assign everything we've loaded to the oak singleton
 Object.assign(oak, exports);
 
