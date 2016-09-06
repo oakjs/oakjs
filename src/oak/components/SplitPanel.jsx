933536da3e25415596526dad272e3aabe3503288
owenoak <owenoak@gmail.com>
2016-09-06 13:10:39 -0400

    SplitPanel appearance

--- src/oak/components/SplitPanel.jsx
+++ src/oak/components/SplitPanel.jsx
@@ -40,6 +40,12 @@ export default class SplitPanel extends OakComponent {
     // Can the user resize the panes?
     resizable: PropTypes.bool,
 
+    // Appearance:  "inverted", "bordered"
+    appearance: PropTypes.oneOfType([
+      PropTypes.string,
+      PropTypes.arrayOf(PropTypes.string)
+    ]),
+
     // Ratio of components, eg: `[ "50%", "50%" ]` or `[ 30, "100%" ]`
     sizes: PropTypes.array
   }
@@ -66,7 +72,7 @@ export default class SplitPanel extends OakComponent {
     if (!sizes) return;
 
     const $root = $(ReactDOM.findDOMNode(this));
-    const $children = $root.children(":not(.oak.Splitter)");
+    const $children = $root.children(":not(.oak.divider)");
 
     sizes.forEach( (size, index) => {
       const child = $children[index];
@@ -80,7 +86,7 @@ export default class SplitPanel extends OakComponent {
   }
 
   mungeChildren(props) {
-    const { resizable, scrolling } = props;
+    const { resizable, scrolling, appearance } = props;
     let children = Children.toArray(props.children);
 
     // if scrolling, wrap children in a "Scroller" div
@@ -88,12 +94,13 @@ export default class SplitPanel extends OakComponent {
       children = children.map( child => <div className="oak Scroller">{child}</div> );
     }
 
-    // if resizable, interleave <Splitter> divs.
-    if (resizable) {
+    // if resizable, interleave `<div.oak.divider>` divs.
+    if (resizable || (appearance && appearance.includes("bordered"))) {
       const interleaved = children.map( (child, index) => {
         if (index === 0) return child;
-        return [<div className="oak Splitter"/>, child];
+        return [<div className="oak divider"/>, child];
       });
+      // flatten the interleaved list
       children = [].concat(...interleaved);
     }
 
@@ -102,11 +109,12 @@ export default class SplitPanel extends OakComponent {
 
   getRenderProps(props) {
     props = { ...props };
-    let { resizable, scrolling, direction, className } = props;
+    let { appearance, className, direction, resizable, scrolling, } = props;
 
     // Add standard classNames to those passed in with `props`
     props.className = classNames(
       "oak",
+      appearance,
       { scrolling, resizable },
       direction,
       "SplitPanel",
