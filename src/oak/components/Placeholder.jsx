37c6a243078d1c5fcae992aa3f2b31720ccceb0d
owenoak <owenoak@gmail.com>
2016-09-09 19:40:56 -0400

    nifty SVG <Placeholder> component

---
+++ src/oak/components/Placeholder.jsx
@@ -0,0 +1,27 @@
+//////////////////////////////
+//  Placeholder component -- shows as an `[X]` to represent some un-drawable component.
+//////////////////////////////
+
+import "./Placeholder.less";
+
+export default function Placeholder(props, context) {
+  const { label } = props;
+  return (
+    <div className="oak Placeholder">
+      <div className="inset">
+        <svg width="100%" height="100%" viewBox="0 0 100 100" preserveAspectRatio="none">
+          <path width="100" height="100"
+            transform="matrix(1 0 0 1 0.5 0.5)"
+            strokeDasharray="5,5" strokeWidth="1.5" vectorEffect="non-scaling-stroke"
+            stroke="#000" fill="none"
+            d="M0,0 L99,0 L99,99 L0,99 L0,0 M0,0 L99,99 M0,99 L99,0" />
+        </svg>
+      </div>
+      <label>{label}</label>
+    </div>
+  );
+}
+
+// Oak editor prefs
+import { editify } from "../EditorProps";
+editify({ draggable: true, droppable: false }, Placeholder);
