5d888f0aff1bb84905d58124ba312005514d1142
owenoak <owenoak@gmail.com>
2016-09-19 17:47:28 -0400

    mocha-test correctly uses babel

--- test/test-Point.js
+++ test/test-Point.js
@@ -1,12 +1,7 @@
-//import Point from "../src/oak-roots/Point";
-
-//Point  = require('../src/oak-roots/Point');
-//const pointInstance = new Point();
-
 var chai   = require('chai');
 var expect = chai.expect;
 
-var Point = require('../src/oak-roots/Point');
+import Point from "../src/oak-roots/Point";
 
 describe('The Point class', function(){
   it('will return no error if the class exists', function(){
