70c99f35a35d4c1b7ea26ee58690e0423077ae91
owenoak <owenoak@gmail.com>
2016-09-19 12:08:26 -0400

    project | section | page | component server api code refactor

--- src/server/api.js
+++ src/server/api.js
@@ -60,6 +60,59 @@ function saveTextFile(request, response, path, body) {
 
 
 //////////////////////////////
+//  Generic `Component` class actions.
+//  All assume you pass in an instance of the server `Component` or subclass.
+//////////////////////////////
+
+// Handle GET request action for a component.
+function handleComponentGetAction(action, component, request, response) {
+  switch (action) {
+    case "bundle":  return component.getBundle(response, request.query.force === "true");
+    case "jsxe":    return component.getJSXE(response);
+    case "script":  return component.getScript(response);
+    case "styles":  return component.getStyles(response);
+    case "index":   return component.getChildIndex(response);
+    case "compile": return component.getCompiled(response);
+  }
+  throw new TypeError(`${component.type} GET API action '${action}' not defined.`);
+}
+
+
+
+// Handle POST request action for a component.
+function handleComponentPostAction(action, component, request, response) {
+  const { body } = request;
+  const data = body ? JSON.parse(body) : {};
+  switch (action) {
+    case "save":      return component.save(data)
+                        .then( () => component.getBundle(response) )
+                        .catch( error => { console.error(error); throw new Error(error)} );
+
+    case "create":    return component.create(data)
+                        .then( () => component.getBundleAndParentIndex(response) )
+                        .catch( error => { console.error(error); throw new Error(error)} );
+
+    case "duplicate": return component.duplicate(data)
+                        .then( newComponent => newComponent.getBundleAndParentIndex(response) )
+                        .catch( error => { console.error(error); throw new Error(error)} );
+
+    case "rename":    return component.changeId(data)
+                        .then( newComponent => newComponent.parentIndex.getFile(response) )
+                        .catch( error => { console.error(error); throw new Error(error)} );
+
+    case "delete":    return component.delete()
+                        .then( () => component.parentIndex.getFile(response) )
+                        .catch( error => { console.error(error); throw new Error(error)} );
+
+    case "undelete":  return component.undelete(data)
+                        .then( () => component.getBundleAndParentIndex(response) )
+                        .catch( error => { console.error(error); throw new Error(error)} );
+  }
+  throw new TypeError(`Project POST API action '${action}' not defined.`);
+}
+
+
+//////////////////////////////
 //  Account actions
 //////////////////////////////
 
@@ -82,48 +135,14 @@ router.get("/account/:action",  (request, response) => {
 router.get("/page/:action/:projectId/:sectionId/:pageId",  (request, response) => {
   const { action, projectId, sectionId, pageId } = request.params;
   const page = new Page({ projectId, sectionId, pageId });
-  switch (action) {
-    case "bundle":  return page.getBundle(response, request.query.force === "true");
-    case "jsxe":    return page.getJSXE(response);
-    case "script":  return page.getScript(response);
-    case "styles":  return page.getStyles(response);
-  }
-  throw new TypeError(`Page GET API action ${action} not defined.`);
+  return handleComponentGetAction(action, page, request, response);
 });
 
 // Page write actions.
 router.post("/page/:action/:projectId/:sectionId/:pageId", bodyTextParser, (request, response) => {
   const { action, projectId, sectionId, pageId } = request.params;
-  const { body } = request;
-  const data = body ? JSON.parse(body) : {};
-
   const page = new Page({ projectId, sectionId, pageId });
-  switch (action) {
-    case "save":      return page.save(data)
-                        .then( () => page.getBundle(response) )
-                        .catch( error => { console.error(error); throw new Error(error)} );
-
-    case "create":    return page.create(data)
-                        .then( () => page.getBundleAndParentIndex(response) )
-                        .catch( error => { console.error(error); throw new Error(error)} );
-
-    case "duplicate": return page.duplicate(data)
-                        .then( newPage => newPage.getBundleAndParentIndex(response) )
-                        .catch( error => { console.error(error); throw new Error(error)} );
-
-    case "rename":    return page.changeId(data)
-                        .then( newPage => newPage.parentIndex.getFile(response) )
-                        .catch( error => { console.error(error); throw new Error(error)} );
-
-    case "delete":    return page.delete()
-                        .then( () => page.parentIndex.getFile(response) )
-                        .catch( error => { console.error(error); throw new Error(error)} );
-
-    case "undelete":  return page.undelete(data)
-                        .then( () => page.getBundleAndParentIndex(response) )
-                        .catch( error => { console.error(error); throw new Error(error)} );
-  }
-  throw new TypeError(`Page POST API action '${action}' not defined.`);
+  return handleComponentPostAction(action, page, request, response);
 });
 
 
@@ -136,49 +155,14 @@ router.post("/page/:action/:projectId/:sectionId/:pageId", bodyTextParser, (requ
 router.get("/section/:action/:projectId/:sectionId",  (request, response) => {
   const { action, projectId, sectionId } = request.params;
   const section = new Section({ projectId, sectionId });
-  switch (action) {
-    case "bundle":  return section.getBundle(response, request.query.force === "true");
-    case "jsxe":    return section.getJSXE(response);
-    case "script":  return section.getScript(response);
-    case "styles":  return section.getStyles(response);
-    case "index":   return section.getChildIndex(response);
-  }
-  throw new TypeError(`Section GET API action '${action}' not defined.`);
+  return handleComponentGetAction(action, section, request, response);
 });
 
 // Section write actions.
 router.post("/section/:action/:projectId/:sectionId", bodyTextParser, (request, response) => {
   const { action, projectId, sectionId } = request.params;
-  const { body } = request;
-  const data = body ? JSON.parse(body) : {};
-
   const section = new Section({ projectId, sectionId });
-  switch (action) {
-    case "save":      return section.save(data)
-                        .then( () => section.getBundle(response) )
-                        .catch( error => { console.error(error); throw new Error(error)} );
-
-    case "create":    return section.create(data)
-                        .then( () => section.getBundleAndParentIndex(response) )
-                        .catch( error => { console.error(error); throw new Error(error)} );
-
-    case "duplicate": return section.duplicate(data)
-                        .then( newSection => newSection.getBundleAndParentIndex(response) )
-                        .catch( error => { console.error(error); throw new Error(error)} );
-
-    case "rename":    return section.changeId(data)
-                        .then( newSection => newSection.parentIndex.getFile(response) )
-                        .catch( error => { console.error(error); throw new Error(error)} );
-
-    case "delete":    return section.delete()
-                        .then( () => section.parentIndex.getFile(response) )
-                        .catch( error => { console.error(error); throw new Error(error)} );
-
-    case "undelete":  return section.undelete(data)
-                        .then( () => section.getBundleAndParentIndex(response) )
-                        .catch( error => { console.error(error); throw new Error(error)} );
-  }
-  throw new TypeError(`Section POST API action '${action}' not defined.`);
+  return handleComponentPostAction(action, section, request, response);
 });
 
 
@@ -190,49 +174,14 @@ router.post("/section/:action/:projectId/:sectionId", bodyTextParser, (request, 
 router.get("/project/:action/:projectId",  (request, response) => {
   const { action, projectId } = request.params;
   const project = new Project({ projectId });
-  switch (action) {
-    case "bundle":  return project.getBundle(response, request.query.force === "true");
-    case "jsxe":    return project.getJSXE(response);
-    case "script":  return project.getScript(response);
-    case "styles":  return project.getStyles(response);
-    case "index":   return project.getChildIndex(response);
-    case "compile": return project.getCompiled(response);
-  }
-  throw new TypeError(`Project GET API action '${action}' not defined.`);
+  return handleComponentGetAction(action, project, request, response);
 });
 
 // Project write actions.
 router.post("/project/:action/:projectId", bodyTextParser, (request, response) => {
   const { action, projectId } = request.params;
-  const { body } = request;
-  const data = body ? JSON.parse(body) : {};
   const project = new Project({ projectId });
-  switch (action) {
-    case "save":      return project.save(data)
-                        .then( () => project.getBundle(response) )
-                        .catch( error => { console.error(error); throw new Error(error)} );
-
-    case "create":    return project.create(data)
-                        .then( () => project.getBundleAndParentIndex(response) )
-                        .catch( error => { console.error(error); throw new Error(error)} );
-
-    case "duplicate": return project.duplicate(data)
-                        .then( newProject => newProject.getBundleAndParentIndex(response) )
-                        .catch( error => { console.error(error); throw new Error(error)} );
-
-    case "rename":    return project.changeId(data)
-                        .then( newProject => newProject.parentIndex.getFile(response) )
-                        .catch( error => { console.error(error); throw new Error(error)} );
-
-    case "delete":    return project.delete()
-                        .then( () => project.parentIndex.getFile(response) )
-                        .catch( error => { console.error(error); throw new Error(error)} );
-
-    case "undelete":  return project.undelete(data)
-                        .then( () => project.getBundleAndParentIndex(response) )
-                        .catch( error => { console.error(error); throw new Error(error)} );
-  }
-  throw new TypeError(`Project POST API action '${action}' not defined.`);
+  return handleComponentPostAction(action, project, request, response);
 });
 
 
@@ -241,51 +190,16 @@ router.post("/project/:action/:projectId", bodyTextParser, (request, response) =
 //////////////////////////////
 router.get("/component/:action/:projectId/:componentId",  (request, response) => {
   const { action, projectId, componentId } = request.params;
-// UGH... don't want to have to create a Component subclass for each variant...
   const component = new ProjectComponent({ projectId, componentId });
-  switch (action) {
-    case "bundle":  return component.getBundle(response, request.query.force === "true");
-    case "jsxe":    return component.getJSXE(response);
-    case "script":  return component.getScript(response);
-    case "styles":  return component.getStyles(response);
-    case "index":   return component.getChildIndex(response);
-  }
-  throw new TypeError(`Project GET API action '${action}' not defined.`);
+  return handleComponentGetAction(action, component, request, response);
 });
 
 
 // Project write actions.
 router.post("/component/:action/:projectId/:componentId", bodyTextParser, (request, response) => {
   const { action, projectId, componentId } = request.params;
-  const { body } = request;
-  const data = body ? JSON.parse(body) : {};
   const component = new ProjectComponent({ projectId, componentId });
-  switch (action) {
-    case "save":      return component.save(data)
-                        .then( () => component.getBundle(response) )
-                        .catch( error => { console.error(error); throw new Error(error)} );
-
-    case "create":    return component.create(data)
-                        .then( () => component.getBundleAndParentIndex(response) )
-                        .catch( error => { console.error(error); throw new Error(error)} );
-
-    case "duplicate": return component.duplicate(data)
-                        .then( newProject => newProject.getBundleAndParentIndex(response) )
-                        .catch( error => { console.error(error); throw new Error(error)} );
-
-    case "rename":    return component.changeId(data)
-                        .then( newProject => newProject.parentIndex.getFile(response) )
-                        .catch( error => { console.error(error); throw new Error(error)} );
-
-    case "delete":    return component.delete()
-                        .then( () => component.parentIndex.getFile(response) )
-                        .catch( error => { console.error(error); throw new Error(error)} );
-
-    case "undelete":  return component.undelete(data)
-                        .then( () => component.getBundleAndParentIndex(response) )
-                        .catch( error => { console.error(error); throw new Error(error)} );
-  }
-  throw new TypeError(`Project Component POST API action '${action}' not defined.`);
+  return handleComponentPostAction(action, component, request, response);
 });
 
 
