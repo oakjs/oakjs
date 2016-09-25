0e5116ae59aed45f50bae09632665b631e96e6ea
owenoak <owenoak@gmail.com>
2016-09-25 12:46:25 -0400

    comments

--- src/components/index.js
+++ src/components/index.js
@@ -8,20 +8,28 @@
 //  For each "package" of components you want to be available to all projects in this install:
 //    1) Install the components (or a symbolic link) to them in this directory.
 //
-//    2) Create a "package file" (as `<packageName>.js`) in this directory
+//    2) Create a "dynamic loader" file (as `<packageName>.all.js`) in this directory
+//        which loads each component with a webpack `require.ensure` for each component.
+//        See `./Editor.all.js` for an example.
+//
+//    3) Create a "package file" (as `<packageName>.js`) in this directory
 //       which registers your components as a package.
-//       (see `Editor.js` for a simple example.)
+//       See `./Editor.js` for an example.
 //
-//    3) (optional) In your package file, set up `editorProps` to dictate drag and drop particulars.
-//       (see `SUI.js` for an example).
+//    4) (optional) In your package file, directly import any components
+//       that you KNOW will be needed when your app starts up.
+//       This will include them in the main build, minimizing startup round trips.
+//       See `./Oak.js` for an example.
 //
-//    4) Import your package file below.
+//    5) (optional) In your package file, set up `editorProps` to dictate
+//       drag and drop particulars for your components.
+//       See `./SUI.js` for an example.
+//
+//    6) Import your package file below.
 //
 //  NOTE: This file is included in `src/router/index.js`, which makes webpack bundle it for us.
 //
 //////////////////////////////
-
-import oak from "../oak/oak";
 
 // Built-in oak components.
 import "./Oak.js";
