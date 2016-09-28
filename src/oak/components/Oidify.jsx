68c7cbf6883016da409e6372ed7b83046db96267
owenoak <owenoak@gmail.com>
2016-09-28 15:34:45 -0400

    <Oidify> no longer modifies the DOM!!!

--- src/oak/components/Oidify.jsx
+++ src/oak/components/Oidify.jsx
@@ -1,8 +1,7 @@
 //////////////////////////////
 // <Oidify>
 //
-//  Wrapper class to assign `oakid` to its one and only child.
-//  This lets us map from DOM elements back to JSXElements.
+//  Wrapper class to yield pointers to editable JSXElements.
 //  Automatically added to the current `oak.editController`'s widgets.
 //
 //////////////////////////////
@@ -17,63 +16,6 @@ export default class Oidify extends React.Component {
 
   static contextTypes = {
     controller: PropTypes.any,
-  }
-
-  // Return our root element.
-  // No-op if we're not mounted.
-  get element() {
-    if (this._isMounted) return ReactDOM.findDOMNode(this);
-  }
-
-  // Return the current clientRect for our root element as a `Rect`.
-  get clientRect() {
-    return elements.clientRect(this.element);
-  }
-
-
-  assignOid() {
-    const element = this.element;
-    // if no dom node, the component `render()` returned `null`
-    //  or we are loading the actual Component class.  Either way, don't worry about it.
-    if (element === null) return;
-
-    const { oid } = this.props;
-    const controller = this.context.controller;
-
-    if (!oid) return console.warn(`<Oidify>.assignOid(): no oid!`, controller);
-    if (!controller) return console.warn(`<Oidify>.assignOid(${oid}): no controller`);
-
-    // Return if the controller doesn't know about the oid.
-    // This means that the component was drawn by something other than the `editController`,
-    //  e.g. a project component being referenced on the page.
-    const jsxElement = controller.getJSXElementForOid(oid);
-    if (!jsxElement) return;// console.warn(`<Oidify>.assignOid(${oid}): no jsxElement`);
-
-    if (!(element instanceof Element)) {
-      return console.info(`<Oidify>.assignOid(${oid}): oidifying non-element\n`, element, jsxElement);
-    }
-
-    if (element.getAttribute("oakid") !== oid) {
-      element.setAttribute("oakid", oid);
-    }
-  }
-
-  componentDidMount() {
-    this._isMounted = true;
-    this.assignOid();
-  }
-
-  componentWillUpdate() {
-    this._isMounted = false;
-  }
-
-  componentDidUpdate() {
-    this._isMounted = true;
-    this.assignOid();
-  }
-
-  componentWillUnmount() {
-    this._isMounted = false;
   }
 
   render() {
